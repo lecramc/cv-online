3119a170772cb5121c3845bdb8c6f33afb092f77
Clément MARCEL <lecramproject@gmail.com>
2021-07-21 12:29:37 +0200

    add console react component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import Terminal from "react-console-emulator";
+
+const commands = {
+  echo: {
+    description: "Echo a passed string.",
+    usage: "echo <string>",
+    fn: (...args) => args.join(" "),
+  },
+};
+
+class MyTerminal extends Component {
+  render() {
+    return (
+      <Terminal
+        commands={commands}
+        welcomeMessage={"Welcome to the React terminal!"}
+        promptLabel={"clement@marcel:~$"}
+      />
+    );
+  }
 }
-
-export default App;
+export default MyTerminal;
