e9e5bc1a11d8075cf2a37045eb662ee417e8a755
Clément MARCEL <lecramproject@gmail.com>
2021-07-22 09:54:25 +0200

    update path file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import "./App.css";
+import React from "react";
 
-import React, { Component } from "react";
-import Terminal from "react-console-emulator";
+const App = (props) => (
+  <div>
+    <HeaderContainer />
 
-const commands = {
-  echo: {
-    description: "Echo a passed string.",
-    usage: "echo <string>",
-    fn: (...args) => args.join(" "),
-  },
-};
-const styles = {
-  maxHeight: "300px",
-  maxWidth: "720px",
-  margin: "10% auto",
-};
+    <Page data={props.pageStuff} />
 
-class MyTerminal extends Component {
-  render() {
-    return (
-      <Terminal
-        style={styles}
-        commands={commands}
-        welcomeMessage={"Welcome to my home !"}
-        promptLabel={"clement@marcel:~$"}
-      />
-    );
-  }
-}
-export default MyTerminal;
+    <Footer {...props.propsRelevantToFooter} />
+  </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import MyTerminal from "./App";
+import MyTerminal from "./components/Terminal/Terminal";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
