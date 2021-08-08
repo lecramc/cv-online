784c043134089511d5c61b0c5159d636d2acd569
Clement MARCEL <clmtmarcel@gmail.com>
2021-08-08 20:08:20 +0200

    tuto styles is ok

--- src/components/Tutorial/Tutorial.js
+++ src/components/Tutorial/Tutorial.js
@@ -1,66 +1,78 @@
-import "./Tutorial.css";
-
 import { Component } from "react";
 import { Steps, Step } from "react-step-builder";
 import { Button, Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-const Navigation = (props) => {
-  return (
-    <div>
-      {" "}
-      {props.isLast !== true && (
-        <Button id="next" onClick={props.next}>
-          Next
-        </Button>
-      )}
-    </div>
-  );
-};
-
-const config = {
-  navigation: {
-    component: Navigation,
-    location: "after",
+const useStyles = makeStyles({
+  tutorial: {
+    width: "30vw",
+    height: "auto",
+    backgroundColor: "white",
+    padding: "10px",
+    borderRadius: "10px",
+    marginTop: "10px",
+    marginLeft: "10px",
   },
-};
+  next: {
+    color: "#000",
+    backgroundColor: "#77b287",
+  },
+});
 class Tutorial extends Component {
+  constructor(props) {
+    super(props);
+    this.classes = props.classes;
+  }
+  Step1 = (props) => {
+    return (
+      <div>
+        <h3>Welcome to the Curriculum Vitae of Clément MARCEL.</h3>
+        <p>This is a little introduction for use and understand my CV.</p>
+        {props.hasNext() && (
+          <Button className={this.classes.next} onClick={props.next}>
+            Next
+          </Button>
+        )}
+      </div>
+    );
+  };
+  Step2 = (props) => {
+    return (
+      <div>
+        <h3>This is a terminal like a linux bash or cmd in Windows.</h3>
+        <p>You need to use command line for have information about me.</p>
+        {props.hasNext() && (
+          <Button className={this.classes.next} onClick={props.next}>
+            Next
+          </Button>
+        )}
+      </div>
+    );
+  };
+  Step3 = (props) => {
+    return (
+      <div>
+        <p>
+          For beggining use <strong>Clement --help</strong>, all of commands are
+          listed !
+        </p>
+      </div>
+    );
+  };
   render() {
     return (
-      <Box class="tutorial">
-        <Steps config={config}>
-          <Step component={Step1} />
-          <Step component={Step2} />
-          <Step component={Step3} />
+      <Box className={this.classes.tutorial}>
+        <Steps>
+          <Step component={this.Step1} />
+          <Step component={this.Step2} />
+          <Step component={this.Step3} />
         </Steps>
       </Box>
     );
   }
 }
-function Step1(props) {
-  return (
-    <div>
-      <h3>Welcome to the Curriculum Vitae of Clément MARCEL.</h3>
-      <p>This is a little introduction for use and understand my CV.</p>
-    </div>
-  );
-}
-function Step2(props) {
-  return (
-    <div>
-      <h3>This is a terminal like a linux bash or cmd in Windows.</h3>
-      <p>You need to use command line for have information about me.</p>
-    </div>
-  );
-}
-function Step3(props) {
-  return (
-    <div>
-      <p>
-        For beggining use <strong>Clement --help</strong>, all of commands are
-        listed !
-      </p>
-    </div>
-  );
-}
 
-export default Tutorial;
+export default () => {
+  const classes = useStyles();
+  return <Tutorial classes={classes} />;
+};
